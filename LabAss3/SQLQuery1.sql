11bce5c2d751d92b54b16eb582ea7a0337f5aee3
Administrator <Sparkle>
2022-04-22 17:22:30 +0800

    Lab 3 commit

---
+++ LabAss3/SQLQuery1.sql
@@ -0,0 +1,4 @@
+﻿use CustomerDB;
+--insert into Customer(CustomerName, Country, Gender, Hobby, Married) values('Dabing', 'China', 'Male', 'Reading', 'True');
+--insert into Customer(CustomerName, Country, Gender, Hobby, Married)  values('Nishant', 'India', 'Male', 'Reading', 'False');
+select *from Customer;
